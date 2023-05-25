a67215c7ac17c3807b0361d2a9f554ce9fdb4bea
Dannyjar1 <116665399+Dannyjar1@users.noreply.github.com>
2023-05-25 09:21:57 -0500

    Add files via upload

---
+++ PruebaDiagnostica/script.js
@@ -0,0 +1,35 @@
+document.getElementById('reservacion1').addEventListener('submit', function(evento) {
+    evento.preventDefault(); 
+  
+    var nombre = document.getElementById('nombre').value;
+    var inicioFecha = document.getElementById('inicioFecha').value;
+    var finFecha = document.getElementById('finFecha').value;
+  
+    
+  
+    alert('¡Gracias, ' + nombre + '! Tu reserva ha sido realizada del ' + inicioFecha + ' al ' + finFecha + '.');
+  });
+  
+  document.getElementById('reservacion2').addEventListener('submit', function(evento) {
+    evento.preventDefault(); 
+  
+    var nombre = document.getElementById('nombre').value;
+    var inicioFecha = document.getElementById('inicioFecha').value;
+    var finFecha = document.getElementById('finFecha').value;
+  
+    
+  
+    alert('¡Gracias, ' + nombre + '! Tu reserva ha sido realizada del ' + inicioFecha + ' al ' + finFecha + '.');
+  });
+  
+  document.getElementById('reservacion3').addEventListener('submit', function(evento) {
+    evento.preventDefault(); 
+  
+    var nombre = document.getElementById('nombre').value;
+    var inicioFecha = document.getElementById('inicioFecha').value;
+    var finFecha = document.getElementById('finFecha').value;
+  
+    
+  
+    alert('¡Gracias, ' + nombre + '! Tu reserva ha sido realizada del ' + inicioFecha + ' al ' + finFecha + '.');
+  });
